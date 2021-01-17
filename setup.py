ed54c09d134ba6747fa314de336c309ca85070d7
Hesam <hesam.rasouli1@gmail.com>
2021-01-18 00:09:19 +0330

    slight changes

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
    name='py_general_utils',
    version='1.0',
    description='some useful and general utilities in pure python',
    author='Hesam Rasouli',
    author_email='hesam.rasouli1@gmail.com',
-   install_requires=['aiohttp', 'apscheduler']
+   install_requires=['aiohttp', 'apscheduler'],
+   packages=setuptools.find_packages()
 )
