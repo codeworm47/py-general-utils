b69bcb7291e534453b50826a7f8489225430d961
Hesam <hesam.rasouli1@gmail.com>
2021-01-26 20:38:53 +0330

    add iran date to dates.py

--- general_utls/src/types/dates.py
+++ general_utls/src/types/dates.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timezone
+import pytz
 
 
 class Dates:
     @staticmethod
     def now_utc():
         return datetime.now(timezone.utc)
+
+    @staticmethod
+    def now_Iran():
+        return datetime.now(pytz.timezone("IRST"))
