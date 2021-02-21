5668705a0975ef75d1a4e6bc6ed81a2117f28499
Hesam <hesam.rasouli1@gmail.com>
2021-02-22 00:20:21 +0330

    add from_timestamp to dates.py

--- general_utls/src/types/dates.py
+++ general_utls/src/types/dates.py
@@ -8,15 +8,21 @@ class Dates:
     def now_utc():
         return datetime.now(timezone.utc)
 
-    @staticmethod
-    def now():
-        sys_config = Config("settings/system.json")
-        return datetime.now(pytz.timezone(sys_config.read("zone", "datetime")))
+    @classmethod
+    def now(cls):
+        return datetime.now(cls.__get_time_zone())
 
     @staticmethod
     def today_str():
         return datetime.today().strftime('%Y-%m-%d')
 
-    @staticmethod
-    def from_timestamp(time_stamp: float):
-        return datetime.utcfromtimestamp(time_stamp).strftime('%Y-%m-%dT%H:%M:%SZ')
+    @classmethod
+    def from_timestamp(cls, time_stamp: float):
+        sys_config = Config("settings/system.json")
+        return datetime.utcfromtimestamp(time_stamp).replace(tzinfo=cls.__get_time_zone())
+        #.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    @classmethod
+    def __get_time_zone(cls):
+        sys_config = Config("settings/system.json")
+        return pytz.timezone(sys_config.read("zone", "datetime"))
