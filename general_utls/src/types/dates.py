72c12803d3e2f144609029075fda0bd721f885f7
Hesam <hesam.rasouli1@gmail.com>
2021-06-04 02:27:09 +0430

    improvements in dates.py

--- general_utls/src/types/dates.py
+++ general_utls/src/types/dates.py
@@ -18,13 +18,22 @@ class Dates:
         return datetime.today().strftime('%Y-%m-%d')
 
     @classmethod
-    def from_timestamp(cls, time_stamp: float, is_milisec: bool):
+    def from_timestamp(cls, time_stamp: float, is_milisec: bool, append_tz: bool = False):
         if is_milisec:
-            time_stamp = time_stamp/1000
-        return datetime.fromtimestamp(time_stamp, cls.__get_time_zone()).strftime('%Y-%m-%dT%H:%M:%SZ, '
-                                                                                  + str(cls.__get_time_zone()))
-        #.replace(tzinfo=cls.__get_time_zone())
-        #.strftime('%Y-%m-%dT%H:%M:%SZ')
+            time_stamp = time_stamp / 1000
+        date = datetime.fromtimestamp(time_stamp, cls.__get_time_zone())
+        if append_tz:
+            return date.strftime('%Y-%m-%dT%H:%M:%SZ, ' + str(cls.__get_time_zone()))
+        else:
+            return date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        # .replace(tzinfo=cls.__get_time_zone())
+        # .strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    @classmethod
+    def from_timestamp_utc(cls, time_stamp: float, is_milisec: bool):
+        if is_milisec:
+            time_stamp = time_stamp / 1000
+        return datetime.fromtimestamp(time_stamp).strftime('%Y-%m-%dT%H:%M:%SZ')
 
     @classmethod
     def __get_time_zone(cls):
