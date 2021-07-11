a2992c06f62b9c94c7a8958097217706bd951aaf
Hesam <hesam.rasouli1@gmail.com>
2021-07-12 03:21:19 +0430

    change return type of now and now_utc to date instead of str

--- general_utls/src/types/dates.py
+++ general_utls/src/types/dates.py
@@ -5,15 +5,17 @@ import pytz
 
 
 class Dates:
-    UTC_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+    ISO_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
     @classmethod
     def now_utc(cls):
-        return datetime.now(timezone.utc).strftime(cls.UTC_DATE_FORMAT)
+        # return datetime.now(timezone.utc).strftime(cls.ISO_DATE_FORMAT)
+        return datetime.now(timezone.utc)
 
     @classmethod
     def now(cls):
-        return datetime.now(cls.__get_time_zone()).strftime(cls.UTC_DATE_FORMAT)
+        # return datetime.now(cls.__get_time_zone()).strftime(cls.ISO_DATE_FORMAT)
+        return datetime.now(cls.__get_time_zone())
 
     @staticmethod
     def today_str():
@@ -25,9 +27,9 @@ class Dates:
             time_stamp = time_stamp / 1000
         date = datetime.fromtimestamp(time_stamp, cls.__get_time_zone())
         if append_tz:
-            return date.strftime(cls.UTC_DATE_FORMAT+', ' + str(cls.__get_time_zone()))
+            return date.strftime(cls.ISO_DATE_FORMAT + ', ' + str(cls.__get_time_zone()))
         else:
-            return date.strftime(cls.UTC_DATE_FORMAT)
+            return date.strftime(cls.ISO_DATE_FORMAT)
         # .replace(tzinfo=cls.__get_time_zone())
         # .strftime('%Y-%m-%dT%H:%M:%SZ')
 
@@ -35,7 +37,7 @@ class Dates:
     def from_timestamp_utc(cls, time_stamp: float, is_milisec: bool):
         if is_milisec:
             time_stamp = time_stamp / 1000
-        return datetime.fromtimestamp(time_stamp).strftime(cls.UTC_DATE_FORMAT)
+        return datetime.fromtimestamp(time_stamp).strftime(cls.ISO_DATE_FORMAT)
 
     @classmethod
     def hh_mm(cls, date):
