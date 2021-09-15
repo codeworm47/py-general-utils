9dc4de4ad8a6902b4610d8d18ecad53d04ee64b1
Hesam <hesam.rasouli1@gmail.com>
2021-09-16 01:51:07 +0430

    added eval method to booleans

--- general_utls/src/types/booleans.py
+++ general_utls/src/types/booleans.py
@@ -1,6 +1,18 @@
 class Booleans:
     @staticmethod
     def is_bool(value: str):
-        valid_vals = ["true", "false", "0", "1"]
+        valid_vals = ["true", "false", "0", "1", "yes", "no", "y", "n"]
         _value = value.lower()
         return _value in valid_vals
+
+    @classmethod
+    def eval(cls, value: str) -> bool:
+        if not cls.is_bool(value):
+            return False
+        _value = value.lower()
+        trues = ["true", "1", "yes", "y"]
+
+        if _value in trues:
+            return True
+        else:
+            return False
