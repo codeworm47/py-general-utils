2d9898170cae911e759cec90243713a46481d648
hesamr asouli <hesam.rasouli1@gmail.com>
2024-03-10 00:11:53 +0100

    added Numbers.safe_int and Numbers.safe_float and Lists.remove_duplicates

--- general_utls/src/types/lists.py
+++ general_utls/src/types/lists.py
@@ -10,3 +10,7 @@ class Lists:
     def transform(list: List[object], func):
         # assert func is callable
         return [func(li) for li in list]
+
+    @staticmethod
+    def remove_duplicates(list: List[object]):
+        return [i for n, i in enumerate(list) if i not in list[:n]]
--- general_utls/src/types/numbers.py
+++ general_utls/src/types/numbers.py
@@ -1,4 +1,17 @@
+from typing import Optional
+
+
 class Numbers:
     @staticmethod
     def reduce_floating_points(input: float, reduce_count: int = 2):
         return float(f'{input:.{reduce_count}f}')
+
+    def safe_int(self, input: Optional[int]):
+        if not input:
+            return None
+        return int(input)
+
+    def safe_float(self, input: Optional[float]):
+        if not input:
+            return None
+        return float(input)
