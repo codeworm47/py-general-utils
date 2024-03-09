ef7c065c6489010c07aa041a14bfd561e503cab3
hesamr asouli <hesam.rasouli1@gmail.com>
2024-03-10 00:17:13 +0100

    added Numbers.safe_int and Numbers.safe_float and Lists.remove_duplicates

--- general_utls/src/types/numbers.py
+++ general_utls/src/types/numbers.py
@@ -6,12 +6,12 @@ class Numbers:
     def reduce_floating_points(input: float, reduce_count: int = 2):
         return float(f'{input:.{reduce_count}f}')
 
-    def safe_int(self, input: Optional[int]):
+    def safe_int(self, input):
         if not input:
             return None
         return int(input)
 
-    def safe_float(self, input: Optional[float]):
+    def safe_float(self, input):
         if not input:
             return None
         return float(input)
