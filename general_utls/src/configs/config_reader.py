0c6cf8fc8f51b667ae776321397d7cc3eb105886
hesamr asouli <hesam.rasouli1@gmail.com>
2024-05-01 22:25:32 +0200

    refactor Config class to use Dicts.safe_read

--- general_utls/src/configs/config_reader.py
+++ general_utls/src/configs/config_reader.py
@@ -1,8 +1,8 @@
 import logging
 from functools import lru_cache
-from jsonget import json_get
 
 from general_utls.src.io.file_helpers import Files
+from general_utls.src.types.dicts import Dicts
 
 
 class Config:
@@ -19,55 +19,49 @@ class Config:
             logging.error(ex)
             raise ex
 
-    def read(self, key: str, obj_path: str = None):
+    def read(self, key: str, obj_path: str = None, default=None, throw_if_not_found=False):
         try:
             if not key:
                 raise KeyError("key is not provided to read args")
             config = self.__read(self.file_path)
-            if not obj_path:
-                if key not in config:
-                    return None
-                return config[key]
-            else:
-                return json_get(config, self.__get_obj_path(key, obj_path))
+            _key = f"{key}.{obj_path}" if obj_path else key
+            if throw_if_not_found:
+                value = Dicts.safe_read(config, _key)
+                if not value:
+                    raise KeyError(f"key {_key} not found in config {self.file_path}")
+            return Dicts.safe_read(config, _key, default)
         except Exception as ex:
             logging.exception(ex)
             raise ex
 
-    def read_int(self, key: str, obj_path: str = None) -> int:
+    def read_int(self, key: str, obj_path: str = None, default=None, throw_if_not_found=False) -> int:
         try:
-            if not key:
-                raise KeyError("key is not provided to read_int args")
-            str_val = self.read(key, obj_path)
-            return int(str_val)
+            value = self.read(key, obj_path, default, throw_if_not_found)
+            if value:
+                return int(value)
+            return default
         except Exception as ex:
-            logging.exception(ex)
+            logging.error(ex)
             raise ex
 
-    def read_bool(self, key: str, obj_path: str = None) -> bool:
+    def read_bool(self, key: str, obj_path: str = None, default=None, throw_if_not_found=False) -> bool:
         try:
-            if not key:
-                raise KeyError("key is not provided to read_bool args")
-            str_val = self.read(key, obj_path)
-            return bool(str_val)
+            value = self.read(key, obj_path, default, throw_if_not_found)
+            if value:
+                return bool(value)
+            return default
         except Exception as ex:
             logging.exception(ex)
             raise ex
 
-    def read_float(self, key: str, obj_path: str = None) -> float:
+    def read_float(self, key: str, obj_path: str = None, default=None, throw_if_not_found=False) -> float:
         try:
-            if not key:
-                raise ValueError("key is not provided to read_float args")
-            str_val = self.read(key, obj_path)
-            return float(str_val)
+            value = self.read(key, obj_path, default, throw_if_not_found)
+            if value:
+                return float(value)
+            return default
         except Exception as ex:
             logging.exception(ex)
             raise ex
-
-    @staticmethod
-    def __get_obj_path(key: str, obj_path: str):
-        if not obj_path.startswith("/"):
-            return "/" + obj_path + "/" + key
-        return obj_path + "/" + key
 
 
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
    name='py_general_utils',
-   version='1.1.0.6',
+   version='1.2.0.0',
    description='some useful and general utilities in pure python',
    author='Hesam Rasouli',
    author_email='hesam.rasouli1@gmail.com',
-   install_requires=['aiohttp', 'json-get'],
+   install_requires=['aiohttp'],
    packages=setuptools.find_packages()
 )
