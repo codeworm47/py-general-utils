f08331aa0e8292a024d11ad36c4c04e28b3713f7
hesamr asouli <hesam.rasouli1@gmail.com>
2024-06-07 02:51:48 +0200

    added hash_dict to Hash

--- general_utls/src/utils/hash.py
+++ general_utls/src/utils/hash.py
@@ -1,7 +1,16 @@
 from typing import Dict
+import json
+import hashlib
 
 
 class Hash:
     @staticmethod
     def hash_simple_dict(d: Dict):
         return hash(frozenset(d.items()))
+
+    @staticmethod
+    def hash_dict(d: Dict):
+        dict_str = json.dumps(d, sort_keys=True)
+        hash_obj = hashlib.sha256()
+        hash_obj.update(dict_str.encode('utf-8'))
+        return hash_obj.hexdigest()
