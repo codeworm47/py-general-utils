12ea8768674b157d2bba5e1037118523dd32e47c
hesamr asouli <hesam.rasouli1@gmail.com>
2024-12-07 21:43:33 +0100

    Added Parallel class.

--- general_utls/src/parallelism/parallel.py
+++ general_utls/src/parallelism/parallel.py
@@ -3,16 +3,13 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class Parallel:
     @staticmethod
-    def run_parallel(fn, work_items, max_workers, fn_args=None):
-        def get_args(item):
-            args = [item]
-            if fn_args and len(fn_args):
-                args.extend(fn_args)
-            return args
+    def run_parallel(fn, work_items, max_workers, *fn_args):
+        def get_args(item, *args):
+            return [item, *args]
 
         results = []
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            futures = [executor.submit(fn, *get_args(item)) for item in work_items]
+            futures = [executor.submit(fn, *get_args(item, *fn_args)) for item in work_items]
             for future in as_completed(futures):
                 results.append(future.result())
         return results
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
    name='py_general_utils',
-   version='1.2.0.8',
+   version='1.2.0.9',
    description='some useful and general utilities in pure python',
    author='Hesam Rasouli',
    author_email='hesam.rasouli1@gmail.com',
