8ab4ea8a4eb29498d395ce9055c6c5cfbbe40065
hesamr asouli <hesam.rasouli1@gmail.com>
2024-12-26 04:59:17 +0100

    added post to RestClientAsync

--- general_utls/src/http/rest.py
+++ general_utls/src/http/rest.py
@@ -23,3 +23,22 @@ class RestClientAsync:
             async with session.get(url, params=url_params) as response:
                 logging.debug("response status code : %s", response.status)
                 return await response.json()
+
+    @staticmethod
+    async def post(url: str, body: Dict, auth_token: str = None, custom_headers: Dict = None):
+        headers = {}
+
+        msg = f"executing GET call to {url}"
+        if auth_token:
+            msg += f", auth token: {auth_token}"
+            headers['Authorization'] = f'Bearer {auth_token}'
+        if custom_headers:
+            msg += f", custom headers: {custom_headers}"
+            headers.update(custom_headers)
+        logging.debug(msg)
+
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(url, data=body) as response:
+                logging.debug("response status code : %s", response.status)
+                return await response.json()
+
