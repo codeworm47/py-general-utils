13ae551aeeed7b47bec20e85a0c15ad2ec8100dd
Hesam Rasouli <hesam.rasouli1@gmail.com>
2025-01-26 00:03:20 +0100

    troubleshooting

--- general_utls/src/utils/hash.py
+++ general_utls/src/utils/hash.py
@@ -1,7 +1,13 @@
 from datetime import datetime
-from typing import Dict
+from typing import Dict, List
 import json
 import hashlib
+
+
+def date_wise_converter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+    return o
 
 
 class Hash:
@@ -11,12 +17,14 @@ class Hash:
 
     @staticmethod
     def hash_dict(d: Dict):
-        def date_wise_converter(o):
-            if isinstance(o, datetime):
-                return o.__str__()
-            return o
-
         dict_str = json.dumps(d, sort_keys=True, default=date_wise_converter)
         hash_obj = hashlib.sha256()
         hash_obj.update(dict_str.encode('utf-8'))
+        return hash_obj.hexdigest()
+
+    @staticmethod
+    def hash_list(d: List[Dict]):
+        list_str = json.dumps(d, sort_keys=True, default=date_wise_converter)
+        hash_obj = hashlib.sha256()
+        hash_obj.update(list_str.encode('utf-8'))
         return hash_obj.hexdigest()
